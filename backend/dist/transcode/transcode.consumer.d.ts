53a8142fc9192ebca9e841642fe1005633793e30
Ayush Vyas <ayushkv360@gmail.com>
2025-11-26 17:44:27 +0530

    Fix: Remove node_modules and dist from version control

--- backend/dist/transcode/transcode.consumer.d.ts
+++
@@ -1,6 +0,0 @@
-import { Job } from "bullmq";
-export declare class TranscodeConsumer {
-    handleTranscodeJob(job: Job<{
-        fileName: string;
-    }>): Promise<void>;
-}
--- backend/dist/transcode/transcode.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Queue } from 'bullmq';
-export declare class TranscodeService {
-    private readonly transcodeQueue;
-    constructor(transcodeQueue: Queue);
-    transcode(fileName: string): Promise<import("bullmq").Job<any, any, string>>;
-}
